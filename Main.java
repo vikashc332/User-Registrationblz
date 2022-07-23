4baac83985afc21177f81c21da6b3380190fe97b
vikashc332 <85497111+vikashc332@users.noreply.github.com>
2022-07-23 12:37:19 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+package com.blz.UserRegistration;
+
+public class Main {
+    public static void main(String[] args) {
+        UserRegistration userRegistration = new UserRegistration();
+        userRegistration.addUser();
+    }
+}
